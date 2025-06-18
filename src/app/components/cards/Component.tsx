da6aaa75e306242144d34d1e646bcc3946bb0ae6
María <mariamesarojas@gmail.com>
2025-06-17 22:48:44 -0500

    Styles restored to original design

--- src/app/components/cards/Component.tsx
+++ src/app/components/cards/Component.tsx
@@ -6,29 +6,39 @@ interface CardProps {
   icon: string;
   title: string;
   label: string;
+  width?: string;
+  height?: string;
 }
 
 export default function Component() {
   const cards = [
     {
       icon: "dni",
+      width: "88px",
+      height: "53px",
       title: "100% Online",
       label:
         "Identificación y Firma telemática, envío a tu domicilio de tu nuevo Permiso de Circulación.",
     },
     {
       icon: "shield",
+      width: "42px",
+      height: "49px",
       title: "Seguro",
       label:
         "Garantizamos la seguridad del proceso, evita fraudes y/o estafas.",
     },
     {
       icon: "fast",
+      width: "55px",
+      height: "40px",
       title: "Rápido",
       label: "En 15 minutos, habrás tramitado la transferencia.",
     },
     {
       icon: "devices",
+      width: "50px",
+      height: "45px",
       title: "Cómodo",
       label:
         "Sin enviar papeles, sin tener que moverte, desde tu teléfono u ordenador.",
@@ -39,10 +49,10 @@ export default function Component() {
     <div className="w-full flex mb-[69px]">
       {cards.map((card: CardProps, index: number) => {
         return (
-          <div className="flex grow flex-col items-center gap-4 px-6 " key={index}>
-            <img className="h-[3rem]"  src={`/assets/icons/${card.icon}.png`}/>
-            <h4 className="font-bold text-8">{card.title}</h4>
-            <p className="text-base max-w-[250px] text-pretty">{card.label}</p>
+          <div className="flex grow flex-col items-center" key={index}>
+            <img className="mb-[30px]" width={card.width} height={card.height} src={`/assets/icons/${card.icon}.png`}/>
+            <h4 className="font-bold text-8 mb-4">{card.title}</h4>
+            <p className="text-base w-[200px]">{card.label}</p>
           </div>
         );
       })}
